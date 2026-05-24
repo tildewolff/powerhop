a311eeae414accda6d3c54633b701a3c1345b1fb
Tilde <tild2783@stud.kea.dk>
2026-05-25 00:27:03 +0200

    Product page static, responsive

---
+++ src/types/product.ts
@@ -0,0 +1,12 @@
+export interface ProductComparison {
+  id: number;
+  modelname: string;
+  usablepower: number;
+  surgeprotection: number;
+  chargingtime: number;
+  acinputmin: number;
+  acinputmax: number;
+  battery: string;
+  lifespan: number;
+  inverter: number;
+}
